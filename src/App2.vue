542ff89746d0ea604d359a937768f5449ecffec2
Eduardo Tavares <etavaresf@gmail.com>
2016-10-11 07:36:01 -0300

    Novos Componentes

---
+++ src/App2.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <app style="float: left; margin-right: 20px" v-for="c in numbers"></app>
+        <div style="clear: left"></div>
+    </div>
+</template>
+
+<script>
+
+import App from './App.vue'
+
+    export default {
+        name: 'App2',
+        components: {
+            App
+        },
+        mounted(){
+            this.setNumbers(4)
+        },
+        data() {
+            return {
+                numbers: []
+            }
+        },
+        methods: {
+            setNumbers(n) {
+                for (let i = 1; i <= n; i++){
+                    this.numbers.push(i)
+                }
+            }
+        }
+    }
+</script>
---
+++ src/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="left" v-for="c in numbers">
+            <h2>Counter: {{counter}}</h2>
+        </div>
+    </div>
+</template>
+
+<style>
+    .left {
+        float: left; 
+        margin-right: 20px
+    }
+</style>
+
+<script>
+    export default {
+        data() {
+            return {
+                counter: 1,
+                numbers: []
+            }
+        },
+        mounted() {
+            this.setNumbers(500)
+            this.startCount()
+        },
+        methods: {
+            startCount(){
+                setInterval(() => this.counter++, 1);
+            },
+            setNumbers(n) {
+                for (let i = 1; i <= n; i++){
+                    this.numbers.push(i)
+                }
+            }
+        }
+    }
+</script>
